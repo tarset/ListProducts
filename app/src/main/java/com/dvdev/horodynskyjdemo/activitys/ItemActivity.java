d1250db52adfc9663e766872676b3f77f42219af
Ivan Horodynskyj <Ivan Horodynskyj>
2017-03-14 02:23:05 +0200

    Did layout activity_item.xml | Started programming ItemActivity and MainActivity

--- app/src/main/java/com/dvdev/horodynskyjdemo/activitys/ItemActivity.java
+++ app/src/main/java/com/dvdev/horodynskyjdemo/activitys/ItemActivity.java
@@ -1,17 +1,50 @@
 package com.dvdev.horodynskyjdemo.activitys;
 
-import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.dvdev.horodynskyjdemo.R;
 
 public class ItemActivity extends AppCompatActivity {
+    private String INTENT_VALUE;
+
+    private EditText editNameItem;
+    private Button buttonActionCanceNewItem;
+    private Button buttonActionAddNewOrEditedNewItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
+        initializationGlobalObject();
+        //TODO: Потрібно ще ставити по центрі заголовок активності
+
+        INTENT_VALUE = getIntent().getStringExtra(MainActivity.KEY_ACTION);
+        if (INTENT_VALUE.equals(MainActivity.ATTRIBUTE_INTENT_ADD)) {
+            setTitle("Новий продукт");
+            buttonActionAddNewOrEditedNewItem.setText("Додати");
+
+        } else if(INTENT_VALUE.equals(MainActivity.ATTRIBUTE_INTENT_EDIT)) {
+            setTitle("Редагування продукту");
+            buttonActionAddNewOrEditedNewItem.setText("Редагувати");
+
+        }
+    }
+
+    private void initializationGlobalObject() {
+        editNameItem = (EditText) findViewById(R.id.editNameItem);
+        buttonActionCanceNewItem = (Button) findViewById(R.id.cancel_action_new_item);
+        buttonActionAddNewOrEditedNewItem = (Button) findViewById(R.id.confirm_action_new_or_edited_item);
+    }
+
+    public void actionCancelNewItem(View view) {
+        finish();
+    }
+
+    public void actionAddNewOrEditedItem(View view) {
     }
 }
--- app/src/main/java/com/dvdev/horodynskyjdemo/activitys/MainActivity.java
+++ app/src/main/java/com/dvdev/horodynskyjdemo/activitys/MainActivity.java
@@ -6,15 +6,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.dvdev.horodynskyjdemo.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String KEY_ACTION = "action";
+    public static final String ATTRIBUTE_INTENT_ADD = "add";
+    public static final String ATTRIBUTE_INTENT_EDIT = "edit";
+
+    public static String NAME_ITEM = "";
+
+
+
+    private ListView listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializationGlobalObject();
+    }
+
+    private void initializationGlobalObject() {
+        listItem = (ListView) findViewById(R.id.list_item);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.menu_action_add:
                 Intent intent = new Intent(MainActivity.this, ItemActivity.class);
+                intent.putExtra(KEY_ACTION, ATTRIBUTE_INTENT_ADD);
                 startActivity(intent);
                 break;
             case R.id.menu_action_sort:
@@ -39,6 +56,6 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
         }
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 }
